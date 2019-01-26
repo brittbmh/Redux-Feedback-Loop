5c772915a643e0af69b9002b08c6534137b27c33
Britt Carter <brittbmh@gmail.com>
2019-01-26 13:55:32 -0600

    add styling to comment field

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Review from './../Review/Review.js';
 import Button from '@material-ui/core/Button';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { withStyles } from '@material-ui/core/styles';
+import MenuItem from '@material-ui/core/MenuItem';
+import TextField from '@material-ui/core/TextField';
 
 class Comments extends Component {
     constructor() {
@@ -32,7 +37,9 @@ class Comments extends Component {
             <div>
                 <div className="feedbackDiv">
                     <h3>Any additional comments?</h3>
-                    <input type="text" onChange={this.changeComments} placeholder="enter comment" />
+                    <TextField id="standard-full-width" InputLabelProps={{
+                        shrink: true,
+                    }} style={{ margin: 0 }} fullWidth label="Comment" value={this.state.comments} margin="normal" type="text" onChange={this.changeComments} />
                     <br />
                     <br />
                     <Button variant="contained" size="small" color="primary" margin="theme.spacing.unit" className="button" onClick={this.sendComments}>Next</Button>
