4990e2b11dd615210952d26d2e8a05916896e1fa
Britt Carter <brittbmh@gmail.com>
2019-01-28 15:58:01 -0600

    added build folder for Heroku

---
+++ build/precache-manifest.598607e61cca94d5e5bafd910b2daf50.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "65aa3df4e545217d1435",
+    "url": "/static/css/main.978a696c.chunk.css"
+  },
+  {
+    "revision": "65aa3df4e545217d1435",
+    "url": "/static/js/main.65aa3df4.chunk.js"
+  },
+  {
+    "revision": "1cdc6f0c0e765551b91d",
+    "url": "/static/js/1.1cdc6f0c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "040c9357fdea89d77c4c29544f754682",
+    "url": "/index.html"
+  }
+];
