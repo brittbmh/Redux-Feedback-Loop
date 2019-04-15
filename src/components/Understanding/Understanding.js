6ae1f83b4209a744c7f40cdad4f4f0838b413824
Britt Carter <brittbmh@gmail.com>
2019-04-14 20:02:33 -0500

    rating component works in understanding component

--- src/components/Understanding/Understanding.js
+++ src/components/Understanding/Understanding.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Review from './../Review/Review.js';
+import Rating from './../Rating/Rating.js';
 import Button from '@material-ui/core/Button';
-import MenuItem from '@material-ui/core/MenuItem';
-import TextField from '@material-ui/core/TextField';
 
 class Understanding extends Component {
     constructor() {
@@ -14,9 +13,9 @@ class Understanding extends Component {
     }
 
     //update state to capture input
-    changeUnderstanding = (event) => {
+    changeUnderstanding = (rating) => {
         this.setState({
-            understanding: event.target.value
+            understanding: rating
         });
     }
 
@@ -34,23 +33,7 @@ class Understanding extends Component {
             <div>
                 <div className="feedbackDiv">
                     <h3>How well are you understanding the content?</h3>
-                    <TextField
-                        id="outlined-select-rating"
-                        select
-                        label="1 to 5"
-                        className="textField"
-                        value={this.state.understanding}
-                        onChange={this.changeUnderstanding}
-                        helperText="Enter Rating"
-                        margin="normal"
-                        variant="outlined"
-                    >
-                        <MenuItem value="1">1</MenuItem>
-                        <MenuItem value="2">2</MenuItem>
-                        <MenuItem value="3">3</MenuItem>
-                        <MenuItem value="4">4</MenuItem>
-                        <MenuItem value="5">5</MenuItem>
-                    </TextField>
+                    <Rating send={this.changeUnderstanding} />
                     <br />
                     <br />
                     <Button variant="contained" color="primary" margin="theme.spacing.unit" className="button"  onClick={this.sendUnderstanding}>Next</Button>
